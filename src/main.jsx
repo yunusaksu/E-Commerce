7e3ebce3105dd55d5280219d6dbb6ae886a6643b
Yunus AKSU <dev.yunus.aksu@gmail.com>
2025-10-19 23:58:10 +0200

    bearbeitung

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/E-Commerce">
       <App />
     </BrowserRouter>
   </Provider>
